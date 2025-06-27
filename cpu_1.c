3a66b38cf8381cda928681b475a4b19c4369350b
Prathmax45 <prathmeshkanure@gmail.com>
2025-06-27 21:45:56 +0530

    implemented the cpu logics .... half way done with the library file

---
+++ cpu_1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "lib.h"
+#include "cpu.h"
+#include <stdlib.h>
+int A1 = 0 , B1 = 0;
+int cnt_1 = 0;
+int cpu_id_1 = 1;
+
+char memory_cpu_1[MEMORY_SIZE];
+
+void cpu_1(char* filename)
+{
+     FILE *file1 = fopen(filename,"rb");
+     if(!file1)
+     {
+        printf("Unable to open the bin file");
+        exit(0);
+     }
+
+    int i = 0;
+    while(!feof(file1) && i < MEMORY_SIZE)
+    {
+        fread(&memory_cpu_1[i], 1, 1, file1);
+        i++;
+    }
+    fclose(file1);
+
+    while (1)
+    {
+        exe(cnt_1,memory_cpu_1[cnt_1++],&A1,&B1,cpu_id_1);
+    }
+}
---
+++ cpu_2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "lib.h"
+#include "cpu.h"
+#include <stdlib.h>
+int A2  = 0 , B2 = 0;
+int cnt_2 = 0;
+int cpu_id_2 = 2;
+
+char memory_cpu_2[MEMORY_SIZE];
+
+void cpu_2(char* filename)
+{
+     FILE *file2 = fopen(filename,"rb");
+     if(!file2)
+     {
+        printf("Unable to open the bin file");
+        exit(0);
+     }
+
+    int i = 0;
+    while(!feof(file2) && i < MEMORY_SIZE)
+    {
+        fread(&memory_cpu_2[i], 1, 1, file2);
+        i++;
+    }
+    fclose(file2);
+
+    while (1)
+    {
+        exe(cnt_2,memory_cpu_2[cnt_2++],&A2,&B2,cpu_id_2);
+    }
+}
